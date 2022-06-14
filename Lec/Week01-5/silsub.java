14115680d74f8cb92e21650d14e5e30dcd9d1dd6
jayoung <96919844+kjy222@users.noreply.github.com>
2022-06-14 13:26:01 +0900

    Add files via upload

---
+++ Lec/Week01-5/silsub.java
@@ -0,0 +1,22 @@
+package Week01F;
+
+public class silsub {
+    public static void main(String[] args) {
+        // 섞기(Shuffle)
+        // 선생님이 주신 예시다. 분석을 진행해봐야 한다.
+
+        int[] numArr = new int[10];
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = i;
+            System.out.println(numArr[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < 100; i++) {
+            int n = (int)(Math.random()*10);
+            
+        }
+
+
+    }
+}
